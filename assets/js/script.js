fc7e960f9c78a5a2bf5c133d09d63651f8a1f3c3
mkedaraxioned <kedarm@prdxn.com>
2021-04-20 20:58:22 +0530

    modal open and close working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,44 @@
 
 */
 
+$(document).ready(function() {
+    var modal_content = $(".modal-content");
+    var modal_btn = $(".modal-btn");
+    var wrapper= $(".wrapper");
+    var close_btn =$(".close-btn");
+    modal_btn.on("click",openModal);
 
+    close_btn.on("click",closeModal);
+
+    function openModal() {
+        if(modal_content.hasClass("hide-element"))
+        {
+            modal_content.removeClass("hide-element");
+
+            modal_content.addClass("show-element");
+        }
+        if(wrapper.hasClass("opacity-full"))
+        {
+            wrapper.removeClass("opacity-full");
+            wrapper.addClass("opacity-half");       
+        }
+    }
+
+    function closeModal() {
+        if(modal_content.hasClass("show-element"))
+        {
+            modal_content.removeClass("show-element");
+
+            modal_content.addClass("hide-element");
+        }
+        if(wrapper.hasClass("opacity-half"))
+        {
+            wrapper.removeClass("opacity-half");
+            wrapper.addClass("opacity-full");       
+        }
+
+    }
+});
 
 
 
