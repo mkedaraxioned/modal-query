046581bfdc66b5477e3e2947606a59acfe8cd879
mkedaraxioned <kedarm@prdxn.com>
2021-04-21 11:52:54 +0530

    structural changes and on window click close modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,23 +7,31 @@ $(document).ready(function() {
     var modal_btn = $(".modal-btn");
     var wrapper= $(".wrapper");
     var close_btn =$(".close-btn");
-    modal_btn.on("click",openModal);
+    var window_obj=$(window);
+    window_obj.on("click",function(e) {
+    var target_classes = $(e.target).attr('class').split(/\s+/);
+        $.each(target_classes, function(index, item) {
+        if (item == 'wrapper') {
+            closeModal();
+        }
+        });
+    });
+    modal_btn.on("click",function () {
+             if(modal_content.hasClass("hide-element"))
+            {
+                modal_content.removeClass("hide-element");
+    
+                modal_content.addClass("show-element");
+            }
+            if(wrapper.hasClass("opacity-full"))
+            {
+                wrapper.removeClass("opacity-full");
+                wrapper.addClass("opacity-half");       
+            }        
+    });
 
     close_btn.on("click",closeModal);
 
-    function openModal() {
-        if(modal_content.hasClass("hide-element"))
-        {
-            modal_content.removeClass("hide-element");
-
-            modal_content.addClass("show-element");
-        }
-        if(wrapper.hasClass("opacity-full"))
-        {
-            wrapper.removeClass("opacity-full");
-            wrapper.addClass("opacity-half");       
-        }
-    }
 
     function closeModal() {
         if(modal_content.hasClass("show-element"))
